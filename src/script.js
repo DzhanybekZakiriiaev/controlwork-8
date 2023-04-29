8b638293c8c456dd21685271f27b6a02b68d508b
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2023-04-29 12:24:16 +0600

    Improved search

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const resultsContainer = document.getElementById('results');
+const ingContainer = document.getElementById('ingResults');
 
 searchForm.addEventListener('submit', searchHandler);
 
@@ -14,8 +15,27 @@ async function searchCocktails(searchTerm) {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
     const data = await response.json();
     if (data.drinks) {
+        resultsContainer.innerHTML = '<h1>Results by name: </h1>';
         displayCocktails(data.drinks);
+    } else {
+        resultsContainer.innerHTML = 'No cocktails found with this name';
     }
+    const ingResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchTerm}`);
+    const ingData = await ingResponse.json();
+    if (ingData.drinks) {
+        ingContainer.innerHTML = '<h1>Results by ingredient: </h1>';
+        displayCocktailsIng(ingData.drinks);
+    } else {
+        ingContainer.innerHTML = 'No cocktails found with this ingredient';
+    }
+}
+
+function displayCocktailsIng(cocktails) {
+    ingContainer.innerHTML = '';
+    cocktails.forEach(cocktail => {
+        const cocktailElement = createCocktailElement(cocktail);
+        ingContainer.appendChild(cocktailElement);
+    });
 }
 
 function displayCocktails(cocktails) {
@@ -33,9 +53,5 @@ function createCocktailElement(cocktail) {
     <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}">
     <h2>${cocktail.strDrink}</h2>
   `;
-    cocktailElement.addEventListener('click', event => {
-        event.preventDefault();
-        displayCocktailDetails(cocktail);
-    });
     return cocktailElement;
 }
